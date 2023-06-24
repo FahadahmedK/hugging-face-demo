c85c8a82fb73896a05848a9a7e635bcd61ac6dec
Fahad Ahmed <fahad.ahmed95@live.com>
2023-06-24 16:14:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ with gr.Blocks() as demo:
     textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
     gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
+
 demo.launch()
